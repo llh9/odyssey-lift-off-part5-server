cb126b14c221ea7e0adbbd74fab11c022219c0bb
Montii <llh9@yahoo.com>
2022-01-07 20:03:23 -0500

    corrected syntax error

--- src/resolvers.js
+++ src/resolvers.js
@@ -45,10 +45,10 @@ const resolvers = {
       return dataSources.trackAPI.getTrackModules(id);
     },
 
-    duerationInSeconds: ({length}) => length,
+    durationInSeconds: ({length}) => length,
   },
   Module: {
-    duerationInSeconds: ({length}) => length,
+    durationInSeconds: ({length}) => length,
   }
 };
 
